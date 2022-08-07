e10cb604c55310183a407178925f92fc48af7758
Veidz <joaovictorveidz@gmail.com>
2022-08-07 17:29:37 -0300

    feat: content app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Spline from '@splinetool/react-spline';
+import Content from './styles/Content';
 import Wrapper from './styles/Wrapper';
 
 export default function App() {
   return (
     <Wrapper>
       <Spline className='spline' scene="https://prod.spline.design/p9U9HTopRH85UbuS/scene.splinecode" />
+      <Content>
+        <h1>Collaborate with people</h1>
+        <p>Bring your team together and build your community by using our cross-platform app that lets you collaborate via chat, voice and by sharing and storing unlimited media files. A world of topics is waiting for you. Join the private beta.</p>
+      </Content>
     </Wrapper>
   );
 }
