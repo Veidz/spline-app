41e58670c69046244dd72f4b7dd226db302c2238
Veidz <joaovictorveidz@gmail.com>
2022-08-07 19:18:41 -0300

    style: menu component

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,21 @@ import IconLaptop from './assets/icon-laptop.svg'
 import IconTwitter from './assets/icon-twitter.svg'
 import IconYoutube from './assets/icon-youtube.svg'
 import Logo from './assets/logo.svg'
+import Menu from './styles/Menu'
 
 export default function App() {
   return (
     <Wrapper>
       <Spline className='spline' scene="https://prod.spline.design/p9U9HTopRH85UbuS/scene.splinecode" />
       <Content>
+        <Menu>
+          <li><img src={ Logo } alt='Logo' /></li>
+          <li><a href='/'>Home</a></li>
+          <li><a href='/'>Download</a></li>
+          <li><a href='/'>App</a></li>
+          <li><a href='/'>Login</a></li>
+          <li><button>Get Started</button></li>
+        </Menu>
         <h1>Collaborate with people</h1>
         <p>Bring your team together and build your community by using our cross-platform app that lets you collaborate via chat, voice and by sharing and storing unlimited media files. A world of topics is waiting for you. Join the private beta.</p>
         <button>
---
+++ src/styles/Menu.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+const Menu = styled.ul`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+  margin: 0 30px 0 100px;
+  padding: 0;
+
+  li {
+    list-style: none;
+    margin: 0;
+
+    a {
+      text-decoration: none;
+      color: white;
+      padding: 8px 20px;
+      border: 1px solid rgba(255, 255, 255, 0);
+      transition: 1s;
+      border-radius: 14px;
+
+      :hover {
+        border: 1px solid rgba(255, 255, 255, 0.2);
+        cursor: pointer;
+      }
+    }
+  }
+`
+
+export default Menu
